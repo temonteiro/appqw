0e9ce94d0f5164eae1b7f7949aec9956be960942
Thalita Monteiro <thalita.monteiro@ifnet-for.ifactory.com.br>
2016-05-02 10:13:26 -0300

    Arquivos do app

---
+++ www/js/app.js
@@ -0,0 +1,125 @@
+// Ionic Starter App
+
+// angular.module is a global place for creating, registering and retrieving Angular modules
+// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
+// the 2nd parameter is an array of 'requires'
+// 'starter.services' is found in services.js
+// 'starter.controllers' is found in controllers.js
+angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
+
+.run(function($ionicPlatform) {
+  $ionicPlatform.ready(function() {
+    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+    // for form inputs)
+    if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+      cordova.plugins.Keyboard.disableScroll(true);
+
+    }
+    if (window.StatusBar) {
+      // org.apache.cordova.statusbar required
+      StatusBar.styleDefault();
+    }
+  });
+})
+
+.config(function($stateProvider, $urlRouterProvider) {
+
+  // Ionic uses AngularUI Router which uses the concept of states
+  // Learn more here: https://github.com/angular-ui/ui-router
+  // Set up the various states which the app can be in.
+  // Each state's controller can be found in controllers.js
+  $stateProvider
+
+  // setup an abstract state for the tabs directive
+    .state('tab', {
+    url: '/tab',
+    abstract: true,
+    templateUrl: 'templates/tabs.html'
+  })
+
+  // Each tab has its own nav history stack:
+
+  .state('tab.dash', {
+    url: '/dash',
+    views: {
+      'tab-dash': {
+        templateUrl: 'templates/tab-dash.html',
+        controller: 'JobsDaily'
+      }
+    }
+  })
+
+  .state('tab.chats', {
+      url: '/chats',
+      views: {
+        'tab-chats': {
+          templateUrl: 'templates/tab-chats.html',
+          controller: 'BlogDaily'
+        }
+      }
+    })
+    .state('tab.chat-detail', {
+      url: '/chats/:chatId',
+      views: {
+        'tab-chats': {
+          templateUrl: 'templates/chat-detail.html',
+          controller: 'ChatDetailCtrl'
+        }
+      }
+    })
+	.state('tab.jobs-detail', {
+      url: '/jobs/:jobsId',
+      views: {
+        'tab-dash': {
+          templateUrl: 'templates/jobs-detail.html',
+          controller: 'JobDetails'
+        }
+      }
+    })
+	
+	.state('tab.post-detail', {
+      url: '/post/:postId',
+      views: {
+        'tab-chats': {
+          templateUrl: 'templates/post-detail.html',
+          controller: 'PostDetails'
+        }
+      }
+    })
+
+  .state('tab.account', {
+    url: '/account',
+    views: {
+      'tab-account': {
+        templateUrl: 'templates/tab-account.html',
+        controller: 'AccountCtrl'
+      }
+    }
+  });
+
+  // if none of the above states are matched, use this as the fallback
+  $urlRouterProvider.otherwise('/tab/dash');
+
+})
+
+.config(function($ionicConfigProvider) {
+    $ionicConfigProvider.tabs.position('bottom');
+})
+
+.filter('dateFormat', function($filter){
+ return function(input){
+  if(input == null){ return ""; } 
+ 
+  var _date = $filter('date')(new Date(input), 'dd/MM/yy');
+ 
+  return _date.toUpperCase();
+
+ };
+})
+
+.filter("sanitize", ['$sce', function($sce) {
+  return function(htmlCode){
+    return $sce.trustAsHtml(htmlCode);
+  }
+}]);
---
+++ www/js/controllers.js
@@ -0,0 +1,98 @@
+angular.module('starter.controllers', [])
+
+.controller('DashCtrl', function($scope) {})
+
+.controller('ChatsCtrl', function($scope, Chats) {
+  // With the new view caching in Ionic, Controllers are only called
+  // when they are recreated or on app start, instead of every page change.
+  // To listen for when this page is active (for example, to refresh data),
+  // listen for the $ionicView.enter event:
+  //
+  //$scope.$on('$ionicView.enter', function(e) {
+  //});
+
+  $scope.chats = Chats.all();
+  $scope.remove = function(chat) {
+    Chats.remove(chat);
+  };
+})
+
+.controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
+  $scope.chat = Chats.get($stateParams.chatId);
+})
+
+.controller('AccountCtrl', function($scope) {
+  $scope.settings = {
+    enableFriends: true
+  };
+})
+
+.controller('JobsDaily',['$scope', '$http',function($scope, $http){
+	$scope.jobs = {};
+	
+	$http({
+		method:'GET',
+		url: 'http://queroworkar.com.br/blog/wp-json/wp/v2/jobs/?_embed',
+	
+	}).success(function(data){
+		$scope.jobs = data;
+	});
+	
+}])
+
+.controller('JobDetails',['$scope', '$http', '$stateParams', function($scope, $http, $stateParams){
+	$scope.jobs = {};
+	
+	$http({
+		method:'GET',
+		url: 'http://queroworkar.com.br/blog/wp-json/wp/v2/jobs/?_embed',
+	
+	}).success(function(data){
+		
+		for(var i = 0; i < data.length; i++){
+			
+			if(data[i].id === parseInt($stateParams.jobsId)){
+				$scope.jobs = data[i];
+			}
+			
+		}
+	});
+	
+
+}])
+
+.controller('BlogDaily',['$scope', '$http',function($scope, $http){
+	$scope.blog = {};
+	
+	$http({
+		method:'GET',
+		url: 'http://queroworkar.com.br/blog/wp-json/wp/v2/posts/?_embed',
+	
+	}).success(function(data){
+		$scope.blog = data;
+	});
+	
+}])
+
+.controller('PostDetails',['$scope', '$http', '$stateParams', function($scope, $http, $stateParams){
+	$scope.blog = {};
+	
+	$http({
+		method:'GET',
+		url: 'http://queroworkar.com.br/blog/wp-json/wp/v2/posts/',
+	
+	}).success(function(data){
+		
+		for(var i = 0; i < data.length; i++){
+			
+			if(data[i].id === parseInt($stateParams.postId)){
+				$scope.blog = data[i];
+			}
+			
+		}
+	});
+	
+
+}]);
+
+
