1f74f4c4d610913a4cf8ddf02d3109a9d4038996
Samuel Ramon <samuelrbo@gmail.com>
2016-05-09 17:38:07 -0300

    Adicionando funcionalidade pull to refresh

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -7,67 +7,55 @@ angular.module('starter.controllers', [])
 .controller('JobsDaily',['$scope', '$http', '$rootScope', function($scope, $http, $rootScope){
 	$scope.jobs = {};
 
-	$http({
-		method:'GET',
-		url: 'http://queroworkar.com.br/blog/wp-json/wp/v2/jobs/?_embed',
+	var qwHttpRequest = function() {
+			$http({
+				method:'GET',
+				url: 'http://queroworkar.com.br/blog/wp-json/wp/v2/jobs/?_embed',
+			})
+			.success(function(data){
+				$scope.jobs = data;
+				$rootScope.jobs = data;
+			})
+			.finally(function(){
+				$scope.$broadcast('scroll.refreshComplete');
+			});
+	}
 
-	}).success(function(data){
-		$scope.jobs = data;
-		$rootScope.jobs = data;
-	});
+	$scope.doRefresh = function() {
+		qwHttpRequest();
+	}
 
+	qwHttpRequest();
 }])
 
 .controller('JobDetails',['$scope', '$rootScope','$stateParams', function($scope, $rootScope, $stateParams){
-	//$scope.jobs = {};
-
-	//console.log($stateParams);
-	//console.log($rootScope.jobs);
-	//console.log($rootScope.jobs[$stateParams.jobsId]);
-
 	$scope.jobs = $rootScope.jobs[$stateParams.jobsId];
-
-/*
-	$http({
-=======
-  $http({
->>>>>>> master
-		method:'GET',
-		url: 'http://queroworkar.com.br/blog/wp-json/wp/v2/jobs/' + $stateParams.jobsId + '?_embed',
-    cache: true
-
-	}).success(function(data){
-		//$scope.jobs = data;
-	});
-*/
-
 }])
 
 .controller('BlogDaily',['$scope', '$http', '$rootScope',function($scope, $http, $rootScope){
 	$scope.blog = {};
 
-	$http({
-		method:'GET',
-		url: 'http://queroworkar.com.br/blog/wp-json/wp/v2/posts/',
+	var qwHttpRequest = function() {
+			$http({
+				method:'GET',
+				url: 'http://queroworkar.com.br/blog/wp-json/wp/v2/posts/',
+			})
+			.success(function(data){
+				$scope.blog = data;
+				$rootScope.blog = data;
+			})
+			.finally(function(){
+				$scope.$broadcast('scroll.refreshComplete');
+			});
+	}
 
-	}).success(function(data){
-		$scope.blog = data;
-		$rootScope.blog = data;
-	});
+	$scope.doRefresh = function() {
+		qwHttpRequest();
+	}
 
+	qwHttpRequest();
 }])
 
 .controller('PostDetails',['$scope', '$http', '$stateParams', '$rootScope', function($scope, $http, $stateParams, $rootScope){
 	$scope.blog = $rootScope.blog[$stateParams.postId];
-
-	/*$http({
-		method:'GET',
-		url: 'http://queroworkar.com.br/blog/wp-json/wp/v2/posts/' + $stateParams.postId,
-    cache: true
-
-	}).success(function(data){
-    $scope.blog = data;
-	}); */
-
-
 }]);
